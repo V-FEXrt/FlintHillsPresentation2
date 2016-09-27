54233a94f59e9edee64ac7d5cc5bbf01b72d7afc
V-FEXrt <ash123125@yahoo.com>
2016-09-26 22:12:36 -0500

    Pushing the empty project

---
+++ Assets/scripts/SpawnScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+
+	public float spawnRate = 1.0f;
+
+	public Transform spawnPrefab;
+
+	// Use this for initialization
+
+	private float spawnCooldown;
+
+	void Start () {
+		spawnCooldown = spawnRate;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		spawnCooldown -= Time.deltaTime;
+		if (spawnCooldown <= 0) {
+			spawnCooldown = spawnRate;
+
+			var prefab = Instantiate(spawnPrefab) as Transform;
+
+			prefab.position = new Vector3 (10, Random.Range(0.0f, 11.0f));
+		
+		}
+	
+	}
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+Code heavily lifted from this [tutorial by pixelnest.io](http://pixelnest.io/tutorials/2d-game-unity/table-of-contents/)
+
+Project used for a Unity Tutorial by [Ashley Coleman](http://ashleycoleman.me/) for [FlintHillIO](https://www.meetup.com/Flint-Hills-IO/)
